2160be19067c8100930b4c77554308e9c4afdcf2
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-10-15 13:02:18 +0300

    title added

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,11 @@
+import { Row } from "./Index";
+
+const Title = () => {
+  return (
+    <Row>
+      <h1 className="title-menu display-4">CLASS BURGER</h1>
+    </Row>
+  );
+};
+
+export default Title;
