737e67f328942a81dc5d5decb7bbd1bc93f0da7d
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-10-24 16:01:25 +0300

    dispacth fix

--- src/store/contexts/MenuList.js
+++ src/store/contexts/MenuList.js
@@ -6,16 +6,16 @@ export const MenuListContext = createContext();
 
 const MenuListContextProvider = ({ children }) => {
     const initialState = [
-        { id: uuid(), name: "Punda", price: 100, type: "adasd" },
-        { id: uuid(), name: "Manda", price: 1400, type: "adasd" },
-        { id: uuid(), name: "Pandu", price: 600, type: "adasd" },
-        { id: uuid(), name: "Pansu", price: 800, type: "adasd" },
-        { id: uuid(), name: "Panama", price: 900, type: "adasd" },
-        { id: uuid(), name: "Paçenko", price: 100, type: "adasd" },
-        { id: uuid(), name: "Pastırma", price: 500, type: "adasd" },
-        { id: uuid(), name: "Pendırma", price: 700, type: "adasd" },
-        { id: uuid(), name: "Pastırda", price: 100, type: "adasd" },
-        { id: uuid(), name: "Pastırda", price: 100, type: "adasd" },
+        { id: uuid(), name: "Punda", price: 100 },
+        { id: uuid(), name: "Manda", price: 1400 },
+        { id: uuid(), name: "Pandu", price: 600 },
+        { id: uuid(), name: "Pansu", price: 800 },
+        { id: uuid(), name: "Panama", price: 900 },
+        { id: uuid(), name: "Paçenko", price: 100 },
+        { id: uuid(), name: "Pastırma", price: 500 },
+        { id: uuid(), name: "Pendırma", price: 700 },
+        { id: uuid(), name: "Pastırda", price: 100 },
+        { id: uuid(), name: "Pastırda", price: 100 },
     ];
     const [menu, menuDispatch] = useReducer(menuReducer, initialState)
 
